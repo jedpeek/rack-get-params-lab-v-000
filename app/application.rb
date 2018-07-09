2288f5c61f130e4cc50c05008ceb68f407fa28e0
Jed Peek <jedpeek@yahoo.com>
2018-07-09 19:32:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -32,7 +32,7 @@ class Application
       if @@items.include?(search_term)
         resp.write "added #{search_term}"
       else
-        resp.write "We don't have that item"
+        resp.write "We don't have that item\n"
       end
     end
 
